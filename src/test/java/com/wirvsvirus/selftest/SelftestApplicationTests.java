e20d75f776f3ba5982376f44872b58033afe6110
Justus Schneider <jschneider.1997@gmail.com>
2020-03-21 09:45:40 +0100

    Spring Setup

---
+++ src/test/java/com/wirvsvirus/selftest/SelftestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirvsvirus.selftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelftestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
